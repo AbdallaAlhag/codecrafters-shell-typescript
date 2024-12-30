a16fb7fb905c6878a84e6ccef4a82db24c3d750c
Abdalla Alhag <alhagabdalla@gmail.com>
2024-12-29 22:24:50 -0800

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -3,9 +3,12 @@ import { createInterface } from "readline";
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: true,
+
 });
 
 // Uncomment this block to pass the first stage
 rl.question("$ ", (answer) => {
+  console.log(`${answer}: command not found`);
   rl.close();
 });
