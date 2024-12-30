96267ff27e9da277fae88127c0d2112cdbcfda7b
Abdalla Alhag <alhagabdalla@gmail.com>
2024-12-29 22:20:17 -0800

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,9 @@
 import { createInterface } from "readline";
 
-// const rl = createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Uncomment this block to pass the first stage
 rl.question("$ ", (answer) => {
