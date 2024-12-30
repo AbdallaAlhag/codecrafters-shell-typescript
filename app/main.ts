2dc69effe6e2552ebc5d9eddaa4822b2196398b0
Abdalla Alhag <alhagabdalla@gmail.com>
2024-12-29 23:52:11 -0800

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -7,25 +7,24 @@ const rl = createInterface({
 });
 
 function main() {
-  const types: string[] = ["echo", "type"];
+  const types: string[] = ["echo", "type", "exit"];
   rl.question("$ ", (answer: string) => {
     if (answer === "exit 0") {
       rl.close();
+      return;
     }
     if (answer.startsWith("echo ") === true) {
       console.log(answer.slice(5));
-      main();
     } else if (answer.startsWith("type ")) {
       const input: string = answer.slice(5);
       if (types.includes(input)) {
         console.log(`${input} is a shell builtin`);
-        main();
       } else {
         console.log(`${input} is not a shell builtin`);
-        main();
       }
+    } else {
+      console.log(`${answer}: command not found`);
     }
-    console.log(`${answer}: command not found`);
     main();
   });
 }
